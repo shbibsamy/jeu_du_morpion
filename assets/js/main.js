f6a2e35866efc8d5220833b84d2c3821de793cff
shbibsamy <shbibsamy@gmail.com>
2022-05-06 10:14:21 +0200

    Translated to French

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,33 @@
-// Qui joue? 1: premier joueur. -1: deuxieme joueur
-let playerTurn = -1;
+// Qui joue? J1: 1, J2: -1
+let tour = -1;
 
 document.addEventListener("DOMContentLoaded", (event) => {
     init()
 })
 
 function init() {
-    // Get div to store game and how big the game area is
+    // Trouver où on va afficher le jeu
     let tailleGrille = 3;
     let classJeu = document.getElementsByClassName("jeu-grille");
     let espaceJeu = classJeu[0];
-    // Make the game table
+    // Créer le table du jeu
     monTable = createTable(espaceJeu, tailleGrille);
     espaceJeu.appendChild(monTable);
-
 }
 
 function createTable(espaceJeu, taille) {
-    // Create a table and give it a title
+    // Créer le table et lui ajouter un titre
     let monTable = document.createElement("table");
     let monTableCaption = monTable.createCaption();
     monTableCaption.innerHTML = "Morpion";
     monTableCaption.classList.add("morpion-titre");
-    // Create rows and cells
+    // Créer les lignes et les cellules
     for (let rows = 0; rows < taille; rows++) {
         let row = document.createElement("tr");
         for (let cols = 0; cols < taille; cols++) {
             let cell = document.createElement("td");
             cell.id = [rows, cols];
-            // add event listener if player clicks here. This calls the PLAY function and sends this cell's ID.
+            // Ajouter un event listener à chaque cellule. Si cliqué, appel la fonction jouer
             cell.addEventListener("click", (monEvent) => {
                 play(cell.id);
             }, {once : true})
@@ -39,21 +38,8 @@ function createTable(espaceJeu, taille) {
     return monTable;
 }
 
-function play (cell) {
-    playerTurn *= -1;
-    console.log(playerTurn);
+function jouer (cell) {
+    tour *= -1;
+    console.log(tour);
     console.log(cell);
 }
-
-
-
-// document.addEventListener("DOMContentLoaded", function yes() {
-//     let cells = document.querySelectorAll("td");
-//     console.log(cells.length)
-//     for (const elements of cells) {
-//         elements.addEventListener("click", event => {
-//             alert("Clicked on cell");
-//         })
-//     }
-
-// })
